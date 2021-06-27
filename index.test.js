76ffdafe3cc3823b375eeaa113ccc4b29987a41f
Lucas Monteiro <lucas.monteiro@havas.com>
2021-06-27 01:12:47 -0300

    describe

--- index.test.js
+++ index.test.js
@@ -1,25 +1,27 @@
 const applyDiscount = require('./index')
 
-test('Apply discount', () => {
-    expect(applyDiscount(10, 5)).toEqual(5)
-})
+describe('Discount Function', () => {
+    test('Apply discount', () => {
+        expect(applyDiscount(10, 5)).toEqual(5)
+    })
 
-test('Apply discount greater than value', () => {
-    expect(applyDiscount(5, 10)).toEqual(0)
-})
+    test('Apply discount greater than value', () => {
+        expect(applyDiscount(5, 10)).toEqual(0)
+    })
 
-test('Apply negative discount', () => {
-    expect(applyDiscount(10, -5)).toEqual(0)
-})
+    test('Apply negative discount', () => {
+        expect(applyDiscount(10, -5)).toEqual(0)
+    })
 
-test('Apply discount to negative value', () => {
-    expect(applyDiscount(-10, 5)).toEqual(0)
-})
+    test('Apply discount to negative value', () => {
+        expect(applyDiscount(-10, 5)).toEqual(0)
+    })
 
-test('Value are not number', () => {
-    expect(applyDiscount('10', 5)).toBeFalsy()
-})
+    test('Value are not number', () => {
+        expect(applyDiscount('10', 5)).toBeFalsy()
+    })
 
-test('Discount are not number', () => {
-    expect(applyDiscount(10, '5')).toBeFalsy()
+    test('Discount are not number', () => {
+        expect(applyDiscount(10, '5')).toBeFalsy()
+    })
 })
